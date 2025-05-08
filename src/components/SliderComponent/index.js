21f3d980b13be63010cb931916fe2a6ee600e451
Pindi Shiva Kalyan <129607718+gitpsk03@users.noreply.github.com>
2025-05-09 01:22:30 +0530

    Update index.js

--- src/components/SliderComponent/index.js
+++ src/components/SliderComponent/index.js
@@ -28,37 +28,37 @@ const SliderComponent = () => {
             <Slider {...settings} className="carousel">
                 <Link to='/housewarming-gifts'>
                 <div className="slide">
-                    <img className="top-section-img-2" src='https://theoneshop.wpenginepowered.com/wp-content/uploads/2020/09/Potli-Icon.jpg' alt='wedding gifts'/>
+                    <img className="top-section-img-2" src='https://m.media-amazon.com/images/I/71xjyRPnalL._SY695_.jpg' alt='Potli Bags'/>
                     <p className='top-para-2'>Potli Bags</p>
                 </div>
                 </Link>
                 <Link to='/housewarming-gifts'>
                 <div className="slide">
-                <img className="top-section-img-2" src='https://theoneshop.wpenginepowered.com/wp-content/uploads/2020/09/Divine-Icon.jpg' alt='wedding gifts'/>
+                <img className="top-section-img-2" src='https://m.media-amazon.com/images/I/71RABflgNCL._SL1418_.jpg' alt='Devine Gifts'/>
                     <p className='top-para-2'>Devine Gifts</p>
                 </div>
                 </Link>
                 <Link to='/housewarming-gifts'>
                 <div className="slide">
-                <img className="top-section-img-2" src='https://theoneshop.wpenginepowered.com/wp-content/uploads/2017/01/101920-min-768x768.jpg' alt='wedding gifts'/>
+                <img className="top-section-img-2" src='https://m.media-amazon.com/images/I/91WxI+fcA3L._SY695_.jpg' alt='Hand Bags'/>
                 <p className='top-para-2'>Hand Bags</p>
                 </div>
                 </Link>
                 <Link to='/housewarming-gifts'>
                 <div className="slide">
-                <img className="top-section-img-2" src='https://theoneshop.wpenginepowered.com/wp-content/uploads/2015/03/117007B.jpg' alt='wedding gifts'/>
+                <img className="top-section-img-2" src='https://m.media-amazon.com/images/I/813QQudKypL._SL1500_.jpg' alt='Kamadenu'/>
                 <p className='top-para-2'>Kamadenu</p>
                 </div>
                 </Link>
                 <Link to='/housewarming-gifts'>
                 <div className="slide">
-                <img className="top-section-img-2" src='https://theoneshop.wpenginepowered.com/wp-content/uploads/2015/03/102073.jpg' alt='wedding gifts'/>
+                <img className="top-section-img-2" src='https://m.media-amazon.com/images/I/818z8mZLS2L._SL1500_.jpg' alt='Containers'/>
                 <p className='top-para-2'>Containers</p>
                 </div>
                 </Link>
                 <Link to='/housewarming-gifts'>
                 <div className="slide">
-                <img className="top-section-img-2" src='https://theoneshop.wpenginepowered.com/wp-content/uploads/2017/09/147062A-648x648.jpg' alt='wedding gifts'/>
+                <img className="top-section-img-2" src='https://m.media-amazon.com/images/I/61DYKiEmMWL._SL1500_.jpg' alt='Marble Gifts'/>
                 <p className='top-para-2'>Marble Gifts</p>
                 </div>
                 </Link>
